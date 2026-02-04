0cb736ab79c09a0d902ec08baa3f6ffa4ddfa519
Poojadevre <117362783+Poojadevre@users.noreply.github.com>
2026-02-04 13:48:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Online Job Portal Project - Java");
+        System.out.println("Modules: Login | Jobs | Apply");
+    }
+}
