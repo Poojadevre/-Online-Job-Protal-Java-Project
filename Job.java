5b20d5d5dd5c0e706d4898c53a1f072a12b5905d
Poojadevre <117362783+Poojadevre@users.noreply.github.com>
2026-02-04 13:48:17 +0530

    Add files via upload

---
+++ Job.java
@@ -0,0 +1,11 @@
+public class Job {
+    private String title;
+    private String company;
+    private String location;
+
+    public Job(String title, String company, String location) {
+        this.title = title;
+        this.company = company;
+        this.location = location;
+    }
+}
